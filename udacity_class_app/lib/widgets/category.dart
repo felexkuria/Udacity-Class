a52c12c41f4ddfbf4c174e7b9542a8f9c965ce19
felex <engineerfelex@gmail.com>
2021-12-28 06:54:27 +0300

    Intial Commit

---
+++ udacity_class_app/lib/widgets/category.dart
@@ -0,0 +1,75 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// To keep your imports tidy, follow the ordering guidelines at
+// https://www.dartlang.org/guides/language/effective-dart/style#ordering
+import 'package:flutter/material.dart';
+
+/// A custom [Category] widget.
+///
+const _rowHeight = 100.0;
+final _borderRadius = BorderRadius.circular(_rowHeight / 2);
+
+/// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
+/// a colored [InkWell] animation.
+class Category extends StatelessWidget {
+  final IconData iconData;
+  final String string;
+  final Color color;
+
+  /// Creates a [Category].
+  ///
+  /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
+  /// the UI, and the icon that represents it (e.g. a ruler).
+  // TODO: You'll need the name, color, and iconLocation from main.dart
+  const Category({
+    Key? key,
+    required this.iconData,
+    required this.string,
+    required this.color,
+  }) : super(key: key);
+
+  /// Builds a custom widget that shows [Category] information.
+  ///
+  /// This information includes the icon, name, and color for the [Category].
+  @override
+  // This `context` parameter describes the location of this widget in the
+  // widget tree. It can be used for obtaining Theme data from the nearest
+  // Theme ancestor in the tree. Below, we obtain the display1 text theme.
+  // See https://api.flutter.dev/flutter/material/Theme-class.html
+  Widget build(BuildContext context) {
+    // TODO: Build the custom widget here, referring to the Specs.
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        height: _rowHeight,
+        padding: const EdgeInsets.all(8),
+        child: InkWell(
+          borderRadius: _borderRadius,
+          highlightColor: color,
+          splashColor: color,
+          onTap: () {},
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Icon(
+                  iconData,
+                  size: 60.0,
+                ),
+              ),
+              Center(
+                  child: Text(
+                string,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyText1,
+              ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
